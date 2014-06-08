a0c669fa2ea0e00fc716997fabb684c01ce1d386
Dave <dave@Daves-MacBook-Pro.local>
2014-06-07 23:03:41 -0700

    incidents/count is more performant than /index

--- src/js/app.js
+++ src/js/app.js
@@ -8,10 +8,13 @@ var UI = require('ui');
 var Vector2 = require('vector2');
 var Vibe = require('ui/vibe');
 
-
 var user_id = "P304FC8";
 var subdomain = "webdemo";
 var token = "sVVWs84QqzkXqbvVsetM";
+// 2000ms = 2 seconds: about as high as you can get without rate limiting kicking in
+var polling_interval = 2000; 
+
+
 
 var wind = new UI.Window();
 var textfield
@@ -20,11 +23,12 @@ var ajax = require('ajax')
 var count = -1;
 var interval;
 
+// Query the PD API
 PDupdate = function(e) {
   console.log("updating");
   ajax(
     {
-      url: 'http://'+subdomain+'.pagerduty.com/api/v1/incidents?status=triggered,acknowledged&assigned_to_user='+user_id,
+      url: 'http://'+subdomain+'.pagerduty.com/api/v1/incidents/count?status=triggered,acknowledged&assigned_to_user='+user_id,
       type: 'json',
       headers: {
         Authorization: 'Token token='+token,
@@ -33,17 +37,19 @@ PDupdate = function(e) {
     },
     updateScreen,
     function (error) {
-      console.log("Something bad happened :(" + error);
+      console.log("Error:");
+      console.log(error);
     }
   );
 }
 
+// User feedback
 updateScreen = function (data) {      
-  new_count = data.incidents.length || 0
+  new_count = data.total || 0
   console.log("Found " + new_count + " incidents from " + count);
   if(new_count != count) {
     str = ""
-    if(new_count>count) { 
+    if(new_count>count && new_count > 0) { 
       Vibe.vibrate('short'); 
       str = "Oh dear"
     } else if(new_count<count){
@@ -56,10 +62,13 @@ updateScreen = function (data) {
   }
 }
 
-interval = window.setInterval(PDupdate,2000)
+// Polling the API to make the web team cry
 PDupdate()
+interval = window.setInterval(PDupdate,polling_interval)
 
-wind = new UI.Window(); //This probably isn't the best way to clear the screen
+
+// Load a basic screen
+wind = new UI.Window(); 
 textfield = new UI.Text({
   position: new Vector2(0, 50),
   size: new Vector2(144, 30),
@@ -71,6 +80,9 @@ wind.add(textfield);
 wind.show();
 
 
+
+
+// Some basic interactivity
 wind.on('click', 'down', function() {
   textfield.text("Paused, up to resume")
   count = -1
@@ -81,5 +93,5 @@ wind.on('click', 'up', function() {
   textfield.text("Contacting PagerDuty")
   PDupdate()
   clearInterval(interval);
-  interval = window.setInterval(PDupdate,2000)  
+  interval = window.setInterval(PDupdate,polling_interval)  
 });
